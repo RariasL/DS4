d769ef4735553ea027ed7d7a31b4d838264fe2f7
Rodolfo <rodolfoalfredoa@outlook.com>
2025-09-18 19:13:03 -0500

    laboratorio2

---
+++ laboratorio2/laboratorio2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace laboratorio21 { 
+
+
+    public class Program
+     {
+         public static void Main(string[] args)
+         {
+ MyClass.Valor = 2;
+ Console.WriteLine(MyClass.Valor);
+         }
+     }
+     public class MyClass
+     {
+         public static int Valor;
+     }
+ }
---
+++ laboratorio2/laboratorio2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace laboratorio2
+{
+    class Program
+     {
+         static void Main(string[] args)
+         {
+ Client client = new Client();
+ client.FirstName = "Rodolfo";
+ client.LastName = "Arias";
+ client.Age = 30;
+ client.id = 1;
+
+ Console.WriteLine(client.GetFullName());
+         }
+     }
+     public class Client
+     {
+         public int id { get; set; }
+         public string FirstName { get; set; }
+         public string LastName { get; set; }
+         public int Age { get; set; }
+        public string GetFullName()
+         {
+             return FirstName + " " + LastName;
+         }
+     }
+ }
