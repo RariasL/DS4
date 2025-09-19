45816179024d8423f568cc7108f5609f78215a69
Rodolfo <rodolfoalfredoa@outlook.com>
2025-09-18 20:20:02 -0500

    laboratorio4

---
+++ laboratorio4/laboratorio4.4/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la nota del estudiantes");
+        float score = float.Parse(Console.ReadLine());
+
+        if (score >= 70)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Su nota es {score} ha aprobado");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ laboratorio4/laboratorio4/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, x;
+        string linea;
+        Console.Write("Ingrese el valor; ");
+        linea = Console.ReadLine();
+
+        n = int.Parse(linea);
+        x = 1;
+        while (x <= n)
+        {
+            Console.Write(x);
+            Console.WriteLine(",");
+            x = x + 1;
+
+        }
+        Console.ReadKey();
+    }
+}
